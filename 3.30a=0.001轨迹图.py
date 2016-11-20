3cc4e0256d27d4ac68c1e6b210fffe5349839b16
woshishuishuishuishui <296564468@qq.com>
2016-11-20 19:48:37 +0800

    Update 3.30a=0.001轨迹图.py

--- 3.30a=0.001轨迹图.py
+++ 3.30a=0.001轨迹图.py
@@ -1,6 +1,6 @@
 import pylab as pl
 class billiard:
-    def __init__(self,α=0.001,r=1,x0=0.2,y0=0.5,vx0=1/pow(2,1/2),vy0=1/pow(2,1/2),total_time=1000,dt=0.01):
+    def __init__(self,α=0.001,r=1,x0=0.2,y0=0,vx0=0.8,vy0=0.6,total_time=2500,dt=0.01):
         self.α=α
         self.r=r
         self.vx0=vx0
@@ -31,9 +31,9 @@ class billiard:
             return((-2*vn1(x,y,vx,vy)*ny1(x,y))+vy)
         
         def nx2(x,y):
-            return(x/pow(x*x+(y-self.α*self.r)*(y-self.α*self.r),1/2))
+            return(x/pow(x*x+(y+self.α*self.r)*(y+self.α*self.r),1/2))
         def ny2(x,y):
-            return((y-self.α*self.r)/pow(x*x+(y-self.α*self.r)*(y-self.α*self.r),1/2))
+            return((y+self.α*self.r)/pow(x*x+(y+self.α*self.r)*(y+self.α*self.r),1/2))
         def vn2(x,y,vx,vy):
             return(vx*nx2(x,y)+vy*ny2(x,y))
         def vx_after2(x,y,vx,vy):
@@ -54,6 +54,7 @@ class billiard:
                         self.y[-1]=self.y[-2]+self.vy[-2]*(i+1)*self.dt/100
                         self.vx[-1]=(vx_after1(self.x[-1],self.y[-1],self.vx[-2],self.vy[-2]))
                         self.vy[-1]=(vy_after1(self.x[-1],self.y[-1],self.vx[-2],self.vy[-2]))
+                        break
             if self.y[-1]<=-self.α*self.r and judge2(self.x[-1],self.y[-1])>=self.r*self.r:
                 for i in range(100):
                     if judge2(self.x[-2]+self.vx[-2]*(i+1)*self.dt/100,self.y[-2]+self.vy[-2]*(i+1)*self.dt/100)>=self.r*self.r:
@@ -61,19 +62,21 @@ class billiard:
                         self.y[-1]=self.y[-2]+self.vy[-2]*(i+1)*self.dt/100
                         self.vx[-1]=(vx_after2(self.x[-1],self.y[-1],self.vx[-2],self.vy[-2]))
                         self.vy[-1]=(vy_after2(self.x[-1],self.y[-1],self.vx[-2],self.vy[-2]))
+                        break
             if abs(self.y[-1])<self.α*self.r and abs(self.x[-1])>=self.r:
                 for i in range(100):
                     if abs(self.x[-2]+self.vx[-2]*(i+1)*self.dt/100)>=self.r:        
                         self.x[-1]=self.x[-2]+self.vx[-2]*(i+1)*self.dt/100
                         self.y[-1]=self.y[-2]+self.vy[-2]*(i+1)*self.dt/100 
-                        self.vx.append(-self.vx[-1])
-                        self.vy.append(self.vy[-1])
+                        self.vx[-1]=-self.vx[-2]
+                        self.vy[-1]=self.vy[-2]
+                        break
 
-a=billiard()
+a=billiard(α=0.001)
 a.trajectory()
 pl.plot(a.x,a.y)
 pl.xlabel("x")
 pl.ylabel("y")
-pl.xlim(-1,1)
-pl.ylim(-1.02,1.02)
-pl.show()
+pl.grid(True)
+pl.show
+ 
