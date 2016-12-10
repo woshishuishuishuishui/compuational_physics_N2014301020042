c9ee23d97f7ba9d8b26cde4e9266906a730f1e71
woshishuishuishuishui <296564468@qq.com>
2016-12-10 19:40:42 +0800

    Create 5.1 等势面.py

---
+++ 5.1 等势面.py
@@ -0,0 +1,78 @@
+import pylab as pl
+class field:
+    def __init__(self,V0=[],a=[],b=[]):
+        self.V0=V0    
+        self.a=a
+        self.b=b
+        for i in range(401):
+            self.a.append(0)        
+        for j in range(100):
+            self.V0.append(self.a)
+        
+        for i in range(100):
+            self.b.append(0)
+        for i in range(201):
+            b.append(1)
+        for i in range(100):
+            b.append(0)
+        for i in range(201):
+            self.V0.append(self.b)    
+        
+        for i in range(100):
+            self.V0.append(self.a)
+        self.x=[]
+        self.y=[]
+    def update_V(self):
+        Vnew=self.V0
+        for k in range(100):
+            Vold=Vnew
+            Vnew=[self.a]
+            for i in range(399):
+                vtemp=[0]
+                for j in range(399):
+                    vtemp.append((Vold[i][j]+Vold[i+2][j]+Vold[i][j+2]+Vold[i+2][j+2])/4)
+                vtemp.append(0)
+                Vnew.append(vtemp)
+            Vnew.append(self.a)
+            for i in range(201):
+                for j in range(201):
+                    Vnew[i+100][j+100]=1
+
+        
+        for k in range(10000):
+            Vold=Vnew
+            Vnew=[self.a]
+            for i in range(399):
+                vtemp=[0]
+                for j in range(399):
+                    vtemp.append((Vold[i][j]+Vold[i+2][j]+Vold[i][j+2]+Vold[i+2][j+2])/4)
+                vtemp.append(0)
+                Vnew.append(vtemp)
+            Vnew.append(self.a)
+            for i in range(201):
+                for j in range(201):
+                    Vnew[i+100][j+100]=1
+            deltaV=[]
+            for i in range(401):
+                for j in range(401):
+                    deltaV.append(abs(Vnew[i][j]-Vold[i][j]))
+            S=sum(deltaV)
+            if S<0.00001*401*401:
+                V=Vnew
+                print(k)
+                break     
+
+        for k in range(4):
+            for i in range(401):
+                for j in range(401):
+                    if abs(V[i][j]-0.2*(k+1))<0.01:
+                        self.x.append(-2+0.01*i)
+                        self.y.append(2-0.01*j)
+                      
+            
+              
+
+z=field()
+z.update_V()
+pl.plot(z.x,z.y,".")
+pl.show()
