cc748cd0b5f725e2178341f9b4706dcfb56a5f53
woshishuishuishuishui <296564468@qq.com>
2017-01-07 10:45:31 +0800

    Create figure11.6 F的频谱.py

---
+++ figure11.6 F的频谱.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pylab as pl
+import math
+class wave:
+    def __init__(self,L=0.62,FT=650,b=1/8,m=0.0033,K=100000000000,u=0.01,f=262,vh0=0.5):
+        self.L=L
+        self.c=f*2*L
+        self.FT=FT
+        self.dx=L/1000
+        self.dt=self.dx/self.c
+        self.T=10000*self.dt
+        self.b=b
+        self.m=m
+        self.K=K
+        self.u=u
+        self.t=[0]
+        self.y1=[]
+        self.y2=[]
+        self.y3=[]
+        self.y0=[]
+        self.Fh=[]
+        self.zh=[0]
+        self.vh=[vh0]
+        self.F=[]
+        for i in range(int(self.L/self.dx)+1):
+            self.y0.append(0)
+        self.y=[]
+        self.Y=[]
+        self.X=[]
+        for i in range(int(self.L/self.dx)+1):
+            self.X.append(i*self.dx)
+           
+    def propagate(self):
+        self.y1=self.y2=self.y0
+        self.y.append(self.y2[-2])
+        self.F.append(self.FT*(self.y[-1]/self.dx))
+        self.Fh.append(-self.K*pow(self.y2[round((self.L*self.b)/self.dx)]-self.zh[-1],3))
+        self.Y.append(self.y2[round((self.L*self.b)/self.dx)])
+        while self.t[-1]<self.T:
+            self.t.append(self.t[-1]+self.dt)
+            self.y3.append(0)
+            for i in range(round((self.L*self.b)/self.dx)-2):
+                self.y3.append(-self.y1[i+1]+self.y2[i+2]+self.y2[i])
+            self.y3.append(-self.y1[round((self.L*self.b)/self.dx)-1]+self.y2[round((self.L*self.b)/self.dx)]+self.y2[round((self.L*self.b)/self.dx)-2]\
+                                    +0.25*self.dt*self.dt/self.u/self.dx*self.Fh[-1])
+            self.y3.append(-self.y1[round((self.L*self.b)/self.dx)]+self.y2[round((self.L*self.b)/self.dx)+1]+self.y2[round((self.L*self.b)/self.dx)-1]\
+                                    +0.5*self.dt*self.dt/self.u/self.dx*self.Fh[-1])
+            self.y3.append(-self.y1[round((self.L*self.b)/self.dx)+1]+self.y2[round((self.L*self.b)/self.dx)+2]+self.y2[round((self.L*self.b)/self.dx)]\
+                                    +0.25*self.dt*self.dt/self.u/self.dx*self.Fh[-1])
+            for i in range(round(self.L/self.dx)-round((self.L*self.b)/self.dx)-2):
+                self.y3.append(-self.y1[i+1+round((self.L*self.b)/self.dx)+1]+self.y2[i+2+round((self.L*self.b)/self.dx)+1]+self.y2[i+round((self.L*self.b)/self.dx)+1])  
+            self.y3.append(0)
+            self.y.append(self.y3[-2])
+            self.F.append(self.FT*(self.y[-1]/self.dx))
+            self.zh.append(self.zh[-1]+self.vh[-1]*self.dt)
+            self.vh.append(self.vh[-1]-self.Fh[-1]/self.m*self.dt)
+            self.Fh.append(-self.K*pow(self.y2[round((self.L*self.b)/self.dx)]-self.zh[-1],3))
+            self.Y.append(self.y2[round((self.L*self.b)/self.dx)])
+            self.y1=self.y2
+            self.y2=self.y3
+            self.y3=[]
+            if self.y2[round((self.L*self.b)/self.dx)]-self.zh[-1]>0:
+                self.Fh[-1]=0
+                
+a=wave()
+a.propagate()
+P=np.fft.fft(a.F)
+A=[]
+for i in P:
+    A.append(i.real*i.real+i.imag*i.imag)
+frequency=np.linspace(0,a.c/(a.dx),len(P))
+pl.plot(frequency,A)
+pl.xlim(0,2000)
+pl.xlabel("frequency")
+pl.ylabel("power")
+pl.grid(True)
+pl.show()
